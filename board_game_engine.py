1b93cccda00fddbc9bf3e81e45351638c794eb2b
captianjroot <captianjroot@live.com>
2013-08-27 16:00:57 -0500

    Made some functional changes.  Its ugly, but it's smart
    Tweaked "AI" to remove stupid moves.  have not yet implemented boards as
    a class of their own.  added exception handlers to circumvent a few
    problems.  Still working on removing redundancy, improving the code
    layout, and generally pretty-ing things up.

--- board_game_engine.py
+++ board_game_engine.py
@@ -4,9 +4,9 @@ def gameplay(gameboard, p1_move, p2_move, endgame):
     raw_in=input("Who goes first, player 1 or player 2? ")
     first_player=1 if (raw_in=="1" or raw_in=="p1" or raw_in=="player 1") else 2
     while(not End):
-        gameboard=p1_move(gameboard)
+        gameboard=p1_move(gameboard) if (first_player==1) else p2_move(gameboard)
         End=endgame(gameboard)
         if(not End):
-            gameboard=p2_move(gameboard)
+            gameboard=p2_move(gameboard) if (first_player==1) else p1_move(gameboard)
             End=endgame(gameboard)
     return None
--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -4,6 +4,21 @@ import time
 from board_game_engine import gameplay
 from random import randint
 
+def total_nodes(n):
+    return (n*(1+total_nodes(n-1))) if (n>1 and n<995) else 1
+
+def board_conversion(board):
+    solution=[]
+    visual=[part[:] for part in board]
+    for y in range(len(board)):
+        for x in range(len(board[y])):
+            numpad=(7-(3*y))+x
+            if (board[y][x]=="*"):
+                visual[y][x]=str(numpad)
+                solution.append(numpad)
+    return (visual, solution)
+
+
 def toarray(string):
     return [a for a in string]
 
@@ -57,7 +72,7 @@ def sums(board):
         if(len(_sums)>=5):
             _sums[4]+=intboard[a][1]
         else:
-            _sums.append(intboard[a][0])
+            _sums.append(intboard[a][1])
         if(len(_sums)>=6):
             _sums[5]+=intboard[a][2]
         else:
@@ -68,6 +83,7 @@ def sums(board):
     _sums.append(intboard[0][2])
     _sums[7]+=intboard[1][1]
     _sums[7]+=intboard[2][0]
+    #print(_sums)
     return _sums
         
 def game_over(board):
@@ -76,19 +92,19 @@ def game_over(board):
     comp_best=min(scores)
     if(human_best==3):
         winning_pos=scores.index(3)
-        print("The Player Wins!")
+        print("\nThe Player Wins!")
         for a in range(len(board)):
             print("".join(board[a]))
         return True
     elif(comp_best==-3):
         winning_pos=scores.index(-3)
-        print("The Computer Wins!")
+        print("\nThe Computer Wins!")
         for a in range(len(board)):
             print("".join(board[a]))
         return True
     else:
         if(len(moves(board))<1):
-            print("Tie!")
+            print("\nTie!")
             for a in range(len(board)):
                 print("".join(board[a]))
             return True
@@ -96,26 +112,33 @@ def game_over(board):
 
 def human_move(board):
     print("\n\n\n---------------YOUR TURN-----------------")
-    avaliable=moves(board)
-    for a in range(len(board)):
-        print("".join(board[a]))
-    for a in range(len(avaliable)):
-        print(str(a+1)+".   "+str(avaliable[a]))
-    choice=avaliable[int(input("\nWhich move will you make: "))-1]
-    board[choice[0]][choice[1]]="O"
-    print("\n\n\nYour Choice:\n")
-    for a in range(len(board)):
-        print("".join(board[a]))
-    #input("Press enter to continue")
+    gamestate=board_conversion(board)
+    for row in gamestate[0]:
+        print(" ".join(row))
+    valid=False
+    while(not valid):
+        choice=input("\nWhich move will you make: ")
+        for option in gamestate[1]:
+            valid=True if (int(choice)==option) else valid
+    print("\n\nYour Choice:")
+    for y in range(len(gamestate[0])):
+        for x in range(len(gamestate[0][y])):
+            board[y][x]="O" if (gamestate[0][y][x]==str(choice)) else board[y][x]
+    for row in board:
+        print("".join(row))
     return board
 
 def computer_move(board):
     print("\nComputer is thinking.  Please be patient")
     avaliable=moves(board)
     evaluated=[]
+    print("Crunching ", total_nodes(len(avaliable)), " nodes in tree")
     for possibility in avaliable:
         next_board=[thing[:] for thing in board]
         next_board[possibility[0]][possibility[1]]="X"
+        if(min(sums(next_board))==-3):
+            evaluated=[[possibility, 999]]
+            break
         evaluated.append([possibility, branch_eval(next_board, "O")])
         if(evaluated[len(evaluated)-1][1]==1):
             evaluated[len(evaluated)-1][1]=-1000000
@@ -127,6 +150,12 @@ def computer_move(board):
         #print("\n\n")
     evaluated.sort(key=lambda x : x[1])
     evaluated.reverse()
+    try:
+        out=evaluated.pop(evaluated.index(1))
+        evaluated.append(out)
+        evaluated.reverse()
+    except ValueError:
+        pass            
     #computer seems to make valid defensive moves, but tends to pick awkward offensive moves
     if(len(evaluated)==1):
         choice=evaluated[0][0]
@@ -136,7 +165,11 @@ def computer_move(board):
         for pairing in evaluated:
             if(pairing[1]==seek):
                 max_list.append(pairing)
-        choice=evaluated[randint(0, len(max_list))][0]
+        #im only treating the symptoms of a problem here.  until i can identify the problem
+        try:
+            choice=max_list[randint(0, len(max_list))][0]
+        except IndexError:
+            choice=evaluated[0][0]        
     else:
         choice=evaluated[0][0]
     board[choice[0]][choice[1]]="X"
