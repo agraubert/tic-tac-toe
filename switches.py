165b4610ff603c05e4092413ee82cf408573de8c
captianjroot <captianjroot@live.com>
2013-09-13 20:18:29 -0500

    updates switches

--- switches.py
+++ switches.py
@@ -4,8 +4,11 @@ class case:
         self.func=function
 
 def switch(target, *cases, default=None):
+    """(target, *cases, default=None)"""
     for CASE in cases:
         if(target==CASE.val):
             return CASE.func()
     if(type(default)==type(switch)):
         return default()
+    else:
+        return default
